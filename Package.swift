82b8affb2f5cc0ba2a9bf36d9635463d18a31f4a
behring <behring_zhao@126.com>
2023-08-11 22:53:03 +0800

    Revert "Modify Package.swift"
    This reverts commit aac53b445c8214ce99c012fdf023d9c97584cf6c.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
             targets: ["ScrollViewStyle"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/behring/SwiftUI-Introspect.git", branch: "master"),
-        .package(url: "https://github.com/behring/STools.git", branch: "master")
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect", "0.1.4"..<"1.0.0"),
+        .package(url: "https://github.com/TimmysApp/STools", "1.0.74"..<"1.1.0")
     ],
     targets: [
         .target(
